1150037dbcc487c68c9990e443d011809a08d837
Training_H2A.03.20 <Training_h2a.03.20@accenture.com>
2018-10-30 15:41:30 +0530

    Samplejava project

---
+++ src/main/java/firstjenkins/sampleJenkins/App.java
@@ -0,0 +1,14 @@
+package firstjenkins.sampleJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Vijay is Good boy :) ");
+    }
+}
